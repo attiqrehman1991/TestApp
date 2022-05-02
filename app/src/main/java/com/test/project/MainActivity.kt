0f6a2c2bf7ea11b5acbc4c2616ffed30a37fa5bd
Attiq Ur Rehman <attiq.rehman@chilindo.com>
2022-05-02 17:13:37 +0700

    added log2

--- app/src/main/java/com/test/project/MainActivity.kt
+++ app/src/main/java/com/test/project/MainActivity.kt
@@ -9,7 +9,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-        Log.d("Test", "Test")
+        Log.d("Test", "Test2")
 
         // Log.d
     }
